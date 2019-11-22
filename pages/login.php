9ef68dbe1e30360b737489572d9b729099203c18
andersonwagner06 <36700852+andersonwagner06@users.noreply.github.com>
2019-11-21 21:40:09 -0300

    ajustes
    ajustes

---
+++ pages/login.php
@@ -0,0 +1,19 @@
+<?php
+include("includes/conexao.php");
+$login = $_POST['Usuario'];
+$senha = md5($_POST['Senha']);
+
+$sql  = "SELECT * FROM usuario WHERE login = '$login' AND senha = '$senha'";
+
+$query = mysqli_query($serv, $sql);
+
+if (mysqli_num_rows($query) > 0) {
+
+   setcookie("login", $login, time() + 3600);
+   header("Location: ../index.php");
+} else {
+   echo "<script language='javascript' type='text/javascript'>
+        alert('Login e/ou senha incorretos');window.location
+        .href='login.html';</script>";
+   die();
+}
--- views/cadastro_usuario.php
+++ views/cadastro_usuario.php
@@ -1,34 +1,30 @@
-<?php 
-        include("../includes/conexao.php"); 
+<?php
+include("../includes/conexao.php");
 
-        $login = $_POST[Login];
-        $senha = md5($_POST[Senha]);
+$login = $_POST[Login];
+$senha = md5($_POST[Senha]);
 
 $sqlValid  = "SELECT * FROM usuario WHERE login = '$login'";
 
-$queryValid = mysqli_query($serv,$sqlValid); 
+$queryValid = mysqli_query($serv, $sqlValid);
 
-      if (mysqli_num_rows($queryValid)>0){
+if (mysqli_num_rows($queryValid) > 0) {
 
-        echo"<script language='javascript' type='text/javascript'>
+        echo "<script language='javascript' type='text/javascript'>
         alert('Já existe um usuário cadastrado para este login! Por favor informe um outro login.');window.location
-        .href='cadastro_usuario.html';</script>";
-        die(); 
-      }
-      else
-      {
+        .href='../pages/cadastro_usuario.html';</script>";
+        die();
+} else {
         $sql = "INSERT INTO usuario VALUES ( null, 
                                                '$login', 
                                                '$_POST[Nome]',
                                                '$senha'
-                                                )"; 
-        $query = mysqli_query($serv,$sql); 
-        if ($query) 
-                header("Location: listar_usuario.php"); 
-        else 
+                                                )";
+        $query = mysqli_query($serv, $sql);
+        if ($query)
+                header("Location: ../index.php");
+        else
                 echo "<script language='javascript' type='text/javascript'>
         alert('Erro ao inserir registro');window.location
-        .href='cadastro_usuario.html';</script>"; 
-      }
-?>  
-
+        .href='../pages/cadastro_usuario.html';</script>";
+}
